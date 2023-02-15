8428876cbe6064bd7f8a836176d310817c5c7051
Matej Pavic <mpavic_ag@medable.com>
2023-02-15 23:39:05 +0100

    deleting unnecessary or in variable initialization

--- src/getData.js
+++ src/getData.js
@@ -1,6 +1,6 @@
 export default async function getData() {
   //get data from select input
-  const jokeType = document.querySelector('select').value || 'programming';
+  const jokeType = document.querySelector('select').value;
   console.log(jokeType);
   const response = await fetch(`https://v2.jokeapi.dev/joke/${jokeType}?type=twopart`)
   const data = await response.json();
